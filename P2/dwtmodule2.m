cd1178b0e73350d2368746d14321c7da1438f4bd
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-11-10 14:38:36 -0200

    DWT module now shows the signal

--- P2/dwtmodule2.m
+++ P2/dwtmodule2.m
@@ -54,14 +54,19 @@ function dwtmodule2_OpeningFcn(hObject, eventdata, handles, varargin)
 % Choose default command line output for dwtmodule2
 handles.output = hObject;
 handles.signals = {};
+handles.signalnames = {};
 handles.wavelets = [];
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes dwtmodule2 wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
+% ---------------------------------------------------------------------
+function [step] = get_step(signal)
+global fs
+step = 0:1/fs:(length(signal) - 1)/fs;
 
+function context_plot(signal)
+plot(get_step(signal), signal);
 
 % --- Outputs from this function are returned to the command line.
 function varargout = dwtmodule2_OutputFcn(hObject, eventdata, handles)
@@ -82,26 +87,29 @@ function FileMenu_Callback(hObject, eventdata, handles)
 
 % --------------------------------------------------------------------
 function AddMenuItem_Callback(hObject, eventdata, handles)
-% hObject    handle to AddMenuItem (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-file = uigetfile('*.ascii');
-if ~isequal(file, 0)
-    open(file);
+global fa fb fc fs
+[signalname, signalpath] = uigetfile('*.ascii', 'Choose the data file');
+
+if ~isequal(signalname, 0)
+	signal = (load(strcat(signalpath, signalname)) + fa)*fb - fc;
+	signalname = signalname(1:length(signalname)-6);
+
+	handles.signalnames{length(handles.signalnames)+1} = signalname;
+	handles.signals{length(handles.signals)+1} = signal;
+	set(handles.textSignalName, 'String', signalname);
+	if isequal(length(handles.signals), 1)
+		context_plot(signal);
+	end
 end
+
+guidata(hObject, handles);
 
 % --------------------------------------------------------------------
 function PrintMenuItem_Callback(hObject, eventdata, handles)
-% hObject    handle to PrintMenuItem (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 printdlg(handles.figure1)
 
 % --------------------------------------------------------------------
 function CloseMenuItem_Callback(hObject, eventdata, handles)
-% hObject    handle to CloseMenuItem (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 selection = questdlg(['Close DWT module?'],...
                      ['Close DWT module...'],...
                      'Yes','No','Yes');
@@ -138,14 +146,23 @@ end
 
 % --- Executes on selection change in PopupWaveletKind.
 function PopupWaveletKind_Callback(hObject, eventdata, handles)
-% hObject    handle to PopupWaveletKind (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
 % Hints: contents = cellstr(get(hObject,'String')) returns PopupWaveletKind contents as cell array
 %        contents{get(hObject,'Value')} returns selected item from PopupWaveletKind
 contents = cellstr(get(hObject, 'String'));
 wavelet = contents{get(hObject, 'Value')};
+
+% ## Available wavelets ##
+% + Haar
+% + Daubechies
+%   - db1, db2, db3, ..., db10
+% + Symlets
+%   - 2, 3, ..., 8
+% + Coiflets
+%   - 1, 2, ..., 5
+% + Biorthogonal
+%   - 1.1, 1.3, 1.5, 2.2, 2.4, 2.6, 2.8, 3.1, 3.3, 3.5, 3.7, 3.9, 4.4, 5.5, 6.8
+% + R_Biorthigonal
+%   - 1.1, 1.3, 1.5, 2.2, 2.4, 2.6, 2.8, 3.1, 3.3, 3.5, 3.7, 3.9, 4.4, 5.5, 6.8
 
 switch wavelet
 case 'Haar'
@@ -156,18 +173,23 @@ case 'Daubechies'
 case 'Symlets'
 	set(handles.PopupWaveletVar, 'String', ...
 	   {'2' '3' '4' '5' '6' '7' '8'});
+case 'Coiflets'
+	set(handles.PopupWaveletVar, 'String', ...
+	   {'1' '2' '3' '4' '5'});
+case 'Biorthogonal'
+	set(handles.PopupWaveletVar, 'String', ...
+	   {'1.1' '1.3' '1.5' '2.2' '2.4' '2.6' '2.8' ...
+		'3.1' '3.3' '3.5' '3.7' '3.9' '4.4' '5.5' '6.8'});
+case 'R Biorthogonal'
+	set(handles.PopupWaveletVar, 'String', ...
+	   {'1.1' '1.3' '1.5' '2.2' '2.4' '2.6' '2.8' ...
+		'3.1' '3.3' '3.5' '3.7' '3.9' '4.4' '5.5' '6.8'});
 end
 
 guidata(hObject, handles);
 
 % --- Executes during object creation, after setting all properties.
 function PopupWaveletKind_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to PopupWaveletKind (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), ...
 	               get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
@@ -186,12 +208,6 @@ function PopupWaveletVar_Callback(hObject, eventdata, handles)
 
 % --- Executes during object creation, after setting all properties.
 function PopupWaveletVar_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to PopupWaveletVar (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), ...
 	               get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
@@ -210,12 +226,6 @@ function PopupWaveletLevel_Callback(hObject, eventdata, handles)
 
 % --- Executes during object creation, after setting all properties.
 function PopupWaveletLevel_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to PopupWaveletLevel (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), ...
 	               get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
@@ -224,9 +234,6 @@ end
 
 % --- Executes on button press in ButtonCalculate.
 function ButtonCalculate_Callback(hObject, eventdata, handles)
-% hObject    handle to ButtonCalculate (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 contents = get(handles.PopupWaveletKind, 'string')
 
 
