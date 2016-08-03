174865b6805239e6c6c78d02e2851daed3f101e3
Cristiano Silva Jr <ishiikurisu@yahoo.com>
2016-08-03 12:43:03 -0300

    Converting ASCII to WAV

--- etc/voice/ascii2wav.m
+++ etc/voice/ascii2wav.m
@@ -1,10 +1,3 @@
-function ascii2wav(inputname)
-data = [];
-fp = fopen(inputname);
-line = fgetl(fp);
-while ischar(line)
-    data(length(data)+1) = str2num(line);
-    line = fgetl(fp);
-end
-fclose(fp);
-wavwrite(data, 44100);
+function ascii2wav(source)
+data = load(source);
+wavwrite(data, 44100, strcat(source, '.wav'));
