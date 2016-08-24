aebea444720d19f7c3170b1e27e08e06b3079c11
Camila Rosa <rscamila1994@gmail.com>
2016-08-24 11:51:32 -0300

    Minor change so it automatically fills the int field

--- P2/cwtmodule/set_predet_scales.m
+++ P2/cwtmodule/set_predet_scales.m
@@ -5,3 +5,4 @@ maxscale = centfrq(wavetype)/((1/fs)*fmin);
 minscale = centfrq(wavetype)/((1/fs)*fmax);
 set(handles.EditMin, 'String', sprintf('%5.2f',minscale));
 set(handles.EditMax, 'String', sprintf('%5.2f',maxscale));
+set(handles.EditInt, 'String', '1');
