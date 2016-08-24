eb5cd9c659067327821f965b398528453b3c091d
Camila Rosa <rscamila1994@gmail.com>
2016-08-24 11:48:12 -0300

    Changed the way it marks and unmarks when changing modes

--- P2/cwtmodule/disable_predet_opts.m
+++ P2/cwtmodule/disable_predet_opts.m
@@ -2,11 +2,18 @@ function [handles] = disable_predet_opts(hObject, handles)
 	
 set(handles.EditMin, 'Enable', 'on');
 set(handles.EditMax, 'Enable', 'on');
+
 set(handles.RadioDelta, 'Enable', 'off');
+set(handles.RadioDelta, 'Value', 0);
 set(handles.RadioTheta, 'Enable', 'off');
+set(handles.RadioTheta, 'Value', 0);
 set(handles.RadioAlpha, 'Enable', 'off');
+set(handles.RadioAlpha, 'Value', 0);
 set(handles.RadioBeta, 'Enable', 'off');
+set(handles.RadioBeta, 'Value', 0);
 set(handles.RadioGamma, 'Enable', 'off');
+set(handles.RadioGamma, 'Value', 0);
+
 set(handles.RadioManual, 'Value', 0);
 set(handles.RadioPredet, 'Value', 0);
 set(hObject, 'Value', 1);
