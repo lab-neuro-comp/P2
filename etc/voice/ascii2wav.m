976d8c13d2c4be2980bdef2bb303ac898a55db1d
Camila Rosa <rscamila1994@gmail.com>
2016-09-12 09:45:11 -0300

    Começando do zero!!

--- etc/voice/ascii2wav.m
+++
@@ -1,3 +0,0 @@
-function ascii2wav(source)
-data = load(source);
-wavwrite(data, 44100, strcat(source, '.wav'));
--- etc/voice/sigmoid.m
+++
@@ -1,4 +0,0 @@
-function [S] = sigmoid(x)
-% Function to be used during learning process
-S = 2 * x^2 / (1 + x^2); 
-% S = erf(x * sqrt(pi) / 2);
