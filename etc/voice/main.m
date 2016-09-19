f8c18f78f7b42e975fdcc1916a7d4eb2ac4aa9d7
Camila Rosa <rscamila1994@gmail.com>
2016-09-19 11:00:21 -0300

    Using Butterworth Filter Design to generate transfer function

--- etc/voice/main.m
+++ etc/voice/main.m
@@ -2,40 +2,16 @@ function [analysis] = main()
 % Callback to run button
 
 % Loading voice signal
-filename = 'data/voicerecognition.wav';
+filename = 'data/actualcase.wav';
 [record, fs, nbits] = wavread(filename);
-%clc;
 figure;
 plot(1:length(record), record);
 
-%Just trying to see what happens with the old approach
-windowsize = 128;
-[record, queue] = update_queue(record, windowsize);
-windows = [];
 analysis = [];
 n = 1;
 
-while length(queue) > 0
-	% TODO Analyse window
+[b, a] = butter(2, [80, 260]/(fs/2));
+analysis = filter(b, a, record);
 
-	% new lines from here ...
-	[b, a] = butter(2, [80, 260]/(fs/2));
-	% H(s) = s^2/(s^2 + 20*s + 6400)
-	% Transfer function for a bandpass filter with
-	% w0 = 80Hz and w1 = 260 Hz
-	analysis(:, n) = filter(b, a, queue);
-%	if n ==1
-%		figure;
-%		plot(1:length(analysis), analysis);
-%	end
-	% ... till here
-
-	%windows(:, n) = wfft(queue); % WARNING might destroy memory
-	n = n+1; % hehehe
-	[record, queue] = update_queue(record, windowsize);
-end
-
-%figure;
-%surf(real(windows));
 figure;
-surf(analysis);
+plot(analysis);
