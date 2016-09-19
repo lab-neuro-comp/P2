c677049914f4eba29885643a49d4f57b4b3bd592
Camila Rosa <rscamila1994@gmail.com>
2016-09-19 11:31:32 -0300

    Gotta work on update_queue

--- etc/voice/main.m
+++ etc/voice/main.m
@@ -5,14 +5,32 @@ function [analysis] = main()
 % Loading voice signal
 filename = 'data/actualcase.wav';
 [record, fs, nbits] = wavread(filename);
-figure;
-plot(1:length(record), record);
-
 analysis = [];
 n = 1;
 
 [b, a] = butter(2, [80, 260]/(fs/2));
-analysis = filter(b, a, record);
+record = filter(b, a, record);
 
 figure;
+plot(record);
+
+windowsize = 128;
+analysis = [];
+[record, queue] = update_queue(record, windowsize);
+n = 1;
+power = 0;
+
+while length(queue) > 0
+	% TODO Analyse window
+
+	analysis(n) = calc_power(queue);
+	n = n + 1;
+	[record, queue] = update_queue(record, windowsize);
+
+end
+
 plot(analysis);
+
+function [power] = calc_power(spectrum)
+
+power = sum(spectrum.^2);
