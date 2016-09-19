b9cc62bfcf56c5529087430109ebbb8bd00ce89a
Camila Rosa <rscamila1994@gmail.com>
2016-09-19 09:03:32 -0300

    Using Butterworth Filter Design to generate transfer function

--- etc/voice/squarewave.m
+++ etc/voice/squarewave.m
@@ -4,6 +4,20 @@ function [fx] = squarewave(t, w, limit)
 
 K = 1:limit;
 fx = 0;
+sw = [];
 for k = K
 	fx = fx + ((4*sin(2*pi*((2*k)-1)*w*t))/(pi*((2*k)-1)));
+	%sw(k) = fx;
 end
+
+b = [1 0 0];
+a = [1 10*pi (10*pi)^2];
+
+%plot(fx); pause;
+%plot(sw); pause;
+%testanalysis = filter(b, a, sw);
+%plot(testanalysis); pause;
+%disp('Did this work?');
+%disp(t);
+%figure;
+%plot(sw); pause;
--- etc/voice/testing.m
+++ etc/voice/testing.m
@@ -3,7 +3,7 @@ function [analysis] = testing()
 % H(s) = s^2/(s^2 + 20*s + 6400)
 % High pass filter with w0 = 80Hz
 
-T = -10:0.05:10;
+T = 0:0.005:5;
 n = 1;
 w = 1;
 limit = 100;
@@ -14,17 +14,16 @@ for t = T
 	n = n + 1;
 end
 
-[b, a] = transfunc(w);
+%[b, a] = transfunc(2*w);
+[b, a] = butter(2, [1,5]/100);
 analysis = filter(b, a, f);
 figure;
 plot(T, f);
 figure;
 plot(T, analysis);
+fprintf('%d %d\n', length(T), length(analysis));
 
 function [b, a] = transfunc(w)
 
-b = [];
-a = [];
-
-b = [w^2 0 0];
+b = [1 0 0];
 a = [1 w*pi (w*pi)^2];
