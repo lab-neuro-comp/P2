8767dc618886585c631949cad5d69acfb9223b9f
Camila Rosa <rscamila1994@gmail.com>
2016-09-23 11:38:26 -0300

    Minor changes and updated descriptions

--- etc/voice/ignore_noise.m
+++ etc/voice/ignore_noise.m
@@ -1,5 +1,5 @@
 function [corrected] = ignore_noise(signal, threshold)
-% This function will recieve the analysed power
+% Noise Gate: this function will recieve the analysed power
 % and it'll ignore everything that is below the threshold
 
 corrected = zeros(1, length(signal));
@@ -9,7 +9,7 @@ if length(signal) == 0
 	corrected = 0;
 else
 	while length(signal) > 0
-		if signal(1) > threshold
+		if signal(1) >= threshold
 			% corrected(n) = signal(1);
 			corrected(n) = 1;
 		end
--- etc/voice/isvoice.m
+++ etc/voice/isvoice.m
@@ -1,27 +1,28 @@
-function [boolean] = isvoice(signal, powerlimit)
+function [boolean] = isvoice(signal, threshold)
 % Checks where the voice begins
 
 previous = false;
 limit = length(signal);
 boolean = zeros(1, limit);
-threshold = 0;
+%threshold = 0;
 
 for n = 2:limit-1 
 	% This will not analyse the very beginning and the very end
 	% of the signal, but it'll solve the issue of taking a sample
 	% outside of the range of the signal
 
-	if signal(n) > threshold
+	if signal(n) > 0
 		boolean(n) = not(previous);
 		previous = true;
 	else
 		before = signal(n-1);
 		after = signal(n+1);
 
-		if or((before > powerlimit), (after > powerlimit))
+		if or((before > threshold), (after > threshold))
 			% this solve only a pontual problem
 			% if the zero inside the word persist for too long, this is useless
 			boolean(n) = not(previous);
+			previous = true;
 		else
 			previous = false;
 		end
