b893c401802289a71455f904a5b2e0d43c5f6da1
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-09-26 11:10:50 -0300

    Updating upload.bat

--- etc/teste.m
+++
@@ -1,3 +0,0 @@
-notas = input('Introduza as notas: ');
-pesos = input('Introduza os pesos: ');
-media = sum(notas*pesos)/sum(pesos);
--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
-git add .
+git add -A
 git commit
 git push -u origin master
