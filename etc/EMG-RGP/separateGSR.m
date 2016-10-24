b3a8ec332885f5f050ad58883d1e90c1c798f6e3
Camila Rosa <rscamila1994@gmail.com>
2016-10-24 11:52:08 -0200

    It seems like the disturbances were eased a little bit

--- etc/EMG-RGP/separateGSR.m
+++ etc/EMG-RGP/separateGSR.m
@@ -8,4 +8,13 @@ function [EMG, GSR] = separateGSR(edffile)
 % Trying to use the EDF file
 raw = load(asciifile);
 figure;
-plot(raw); % TODO Why is the signal all fucked up?
+hold on;
+plot(raw, 'r'); % TODO Why is the signal all fucked up?
+
+% TODO Find out the sampling frequency
+fs = 2560;
+[b, a] = butter(4, [5, 450]/(fs/2));
+record = filter(b, a, raw);
+plot(record, 'b');
+plot (0:lenght(record), 0, 'g');
+hold off;
