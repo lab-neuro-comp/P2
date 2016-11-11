210bcf90d84aebe5acac666d1beef56b3a7d8cf1
Camila Rosa <rscamila1994@gmail.com>
2016-11-11 10:46:56 -0200

    Turned the time in the file into a timeline

--- etc/voice/analyse_for_stimulus.m
+++ etc/voice/analyse_for_stimulus.m
@@ -10,6 +10,20 @@ timeArray = content{4};
 [R, C] = size(timeArray);
 
 for n = 1:R
-	timeArray{n,1} = split_string(timeArray{n,1}, ':')
+	timeArray{n,1} = split_string(timeArray{n,1}, ':');
 end
 
+% TODO Turn to second
+timeArray{1}{6} = 0;
+for n = 1:R
+	timeArray{n}{5} = (str2num(timeArray{n}{1})*3600 +...
+					   str2num(timeArray{n}{2})*60 +...
+					   str2num(timeArray{n}{3}) +...
+					   str2num(timeArray{n}{4})/1000);
+
+	% Calculates the gap of time from one time to the other
+	if (n > 1)
+		timeArray{n}{6} = (timeArray{n}{5} - timeArray{n-1}{5}) + timeArray{n-1}{6};
+		disp(timeArray{n}{6});
+	end
+end
