fc59d3ed64ac56966fc1d8136e92421cd570f063
Camila Rosa <rscamila1994@gmail.com>
2016-11-14 10:08:03 -0200

    Reading .csv file to compare the timelines

--- etc/voice/analyse_for_stimulus.m
+++ etc/voice/analyse_for_stimulus.m
@@ -1,5 +1,7 @@
-%function [timeArray] = analyse_for_stimulus(filename)
-function [stimulus_time] = analyse_for_stimulus(filename)
+function [stimulusTime] = analyse_for_stimulus(filename)
+% Analyses the file generated during a test with Stroop
+% and gets the time were the stimuli were produced turning them
+% into a timeline in seconds
 
 fileID = fopen(filename, 'r');
 content = textscan(fileID, '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s');
@@ -14,16 +16,30 @@ for n = 1:R
 end
 
 % TODO Turn to second
-timeArray{1}{6} = 0;
+stimulusTime(1) = 0;
 for n = 1:R
-	timeArray{n}{5} = (str2num(timeArray{n}{1})*3600 +...
-					   str2num(timeArray{n}{2})*60 +...
-					   str2num(timeArray{n}{3}) +...
-					   str2num(timeArray{n}{4})/1000);
+	timeArray{n}{5} = (str2num(timeArray{n}{1})*3600 +...	% Hours
+					   str2num(timeArray{n}{2})*60 +...		% Minutes
+					   str2num(timeArray{n}{3}) +...		% Seconds
+					   str2num(timeArray{n}{4})/1000);		% Miliseconds
 
-	% Calculates the gap of time from one time to the other
+	% Calculates the gap of time between stimuli
+	% and creates a timeline
 	if (n > 1)
-		timeArray{n}{6} = (timeArray{n}{5} - timeArray{n-1}{5}) + timeArray{n-1}{6};
-		disp(timeArray{n}{6});
+		stimulusTime(n) = (timeArray{n}{5} - timeArray{n-1}{5}) + stimulusTime(n-1);
 	end
+end
+
+fileID = fopen('data/audio_AnaPaulaRiveiro_Edward2_19.csv');
+timecontent = textscan(fileID, '%s');
+fclose(fileID);
+
+[R, C] = size(timecontent{1})
+
+for n = 1:R
+	timecontent{1}{n} = split_string(timecontent{1}{n}, ';');
+	disp(timecontent{1}{n}{2});
+%	if n > 1
+%		audioTime(n) = timecontent{1}{2,2};
+%	end
 end
