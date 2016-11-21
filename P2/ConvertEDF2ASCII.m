1d239e15b1f91ec30b99b9122c7c51e6942dee0f
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2016-11-21 09:30:48 -0200

    Began replacing the Go EDF lib to the Java EDF lib

--- P2/ConvertEDF2ASCII.m
+++
@@ -1,4 +0,0 @@
-%% ConvertEDF2ASCII: Converts EDF+ files to ASCII files
-function [ok] = ConvertEDF2ASCII(filename, ismultiple)
-addpath(strcat(cd, '/edfp2ascii'));
-ok = translate_edf2ascii(filename, ismultiple);
--- P2/recordmodule/run_for_protolize.m
+++ P2/recordmodule/run_for_protolize.m
@@ -1,10 +1,28 @@
 %% run_for_protolize: Runs the Protolize processing program
 function [errors] = run_for_protolize(handles)
 % handles  the handles variable from the UI
+javaaddpath(strcat(pwd, '/edf.jar'));
 set(handles.buttonRun, 'String', 'Running...');
 stuff = split_string(get(handles.editInput, 'String'), ';');
+errors = false;
 for i = 1:length(stuff)
-    errors = ConvertEDF2ASCII(stuff{i}, get(handles.checkMultiple, 'Value'));
+    multiple = get(handles.checkMultiple, 'Value');
+    errors = errors || ConvertEDF2ASCII(stuff{i}, multiple);
 end
 set(handles.buttonRun, 'String', 'Run');
 msgbox('Done!');
+
+function [errors] = ConvertEDF2ASCII(file, multiple)
+errors = false;
+edf = br.unb.biologiaanimal.edf.EDF(file);
+if multiple
+    % TODO Turn this into multiple files
+else
+    outlet = file;
+    n = length(outlet);
+    while ~isequal(outlet(n), '.')
+        n = n - 1;
+    end
+    outlet = strcat(outlet(1:n), 'ascii');
+    edf.toAscii(outlet);
+end
