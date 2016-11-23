de1b87d9b1ad3cd1b802ae435f1efd0e3e808f3b
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2016-11-23 11:34:10 -0200

    Adding more comments to util functions

--- P2lib/util/needle_in_haystack.m
+++ P2lib/util/needle_in_haystack.m
@@ -1,4 +1,11 @@
 function [index] = needle_in_haystack(haystack, needle)
+% Finds a string in a group of strings.
+%
+%     [index] = needle_in_haystack(haystack, needle)
+%
+% Return the index of the `needle` string in the `haystack` cell array of
+% strings. If the string is not present, index will return as -1.
+%
 index = 1;
 while index <= length(haystack)
     if isequal(haystack{index}, needle)
@@ -6,4 +13,8 @@ while index <= length(haystack)
     else
         index = index + 1;
     end
+end
+
+if index > length(haystack)
+    index = -1;
 end
