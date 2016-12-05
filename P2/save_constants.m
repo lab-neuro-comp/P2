974b7dce20fb20d25a8bc87d80775cab12fc8400
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2016-12-05 09:56:02 -0200

    ???

--- P2/save_constants.m
+++ P2/save_constants.m
@@ -1,4 +1,8 @@
 function save_constants(constants)
+% Save the constants variable to its own file. It is noteworthy that this
+% constants variable is a Java Hashmap obtained from the load_constants function
+% that extracts its data from the constants.config file.
+%
 fp = fopen('constants.config', 'w');
 keys = constants.keySet().toArray();
 
