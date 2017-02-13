2517f64cab9e5a5561ddab3f70e044da950e615e
crs94 <rscamila1994@gmail.com>
2017-02-13 12:15:28 -0200

    Removing the need to create multiple similar files that read the parameters files

--- P2lib/util/ler_arq_ints.m
+++ P2lib/util/ler_arq_ints.m
@@ -4,13 +4,17 @@ function [ints_table] = ler_arq_ints(filepath)
 %
 % It is expected each column represent one data to be used:
 %
-% 1. EDF file
-% 2. Subject identification
-% 3. Condition (that is, the test that is being performed, for example)
-% 4. Classification (for example, their gender)
+% 1. Subject
+% 2. Test
+% 3. Fragment
+% 4. Subject's Initials
 % 5. int1, the lower bound for interval cut
 % 6. int2, the upper bound for interval cut
+% 7. Sampling Rate
+% 8. Session
+% 9. EDF file
 %
+
 [numeric, txt, raw] = xlsread(filepath);
 ints_table = { };
 [y x] = size(raw);
