e07f7d24f5e02b5d9b959f859d4ddd49a675ae23
crs94 <rscamila1994@gmail.com>
2017-03-21 09:19:49 -0300

    Adapted the function for the case of an integer

--- P2lib/util/replace_dot.m
+++ P2lib/util/replace_dot.m
@@ -5,4 +5,7 @@ timestring = [];
 
 temp = num2str(time);
 temp = split_string(temp, '.');
-timestring = char(strcat(temp(1), ',', temp(2)));
+if length(temp) == 1
+	temp{2} = '0';
+end
+timestring = char(strcat(temp{1}, ',', temp{2}));
