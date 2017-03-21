278c727abb32410e96fb406d4c2707b1c35377d5
crs94 <rscamila1994@gmail.com>
2017-03-21 09:18:41 -0300

    Added the mathematics after the files are exported

--- P2/eegmodule/processSingleChan.m
+++ P2/eegmodule/processSingleChan.m
@@ -6,12 +6,37 @@ function [handles] = processSingleChan(handles)
 % Open eeglab:
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
 
-% Reading parameters file
+% Reading data file
 ints_table = ler_arq_ints(get(handles.editTable, 'String'));
 
-% Different approaches for each file extension
 listset = { };
 handles.listset = listset;
+
+% Creating a CSV file to save the parameters
+if get(handles.radioEMG, 'Value')
+    emgParamFile = strcat(get(handles.editOutput, 'String'), '\EMG_Parameters_', date, '.csv');
+    fileID = fopen(emgParamFile, 'w');
+    fprintf(fileID, '%s;%s;%s;%s;%s;%s;%s;%s\n', ...
+                    'Subject', ...
+                    'Condition', ...
+                    'Mean', ...
+                    'Std Deviation', ...
+                    'Variance', ...
+                    'RMS', ...
+                    'Normalized RMS', ...
+                    'Power');
+elseif get(handles.radioEDA, 'Value')
+    edaParamFile = strcat(get(handles.editOutput, 'String'), '\EDA_Parameters_', date, '.csv');
+    fileID = fopen(edaParamFile, 'w');
+    fprintf(fileID, '%s;%s;%s;%s;%s;%s;%s\n', ...
+                    'Subject', ...
+                    'Condition', ...
+                    'Mean', ...
+                    'Std Deviation', ...
+                    'Variance', ...
+                    'SCL', ...
+                    'SCR');
+end
 
 for n = 1:size(ints_table)
     % Prapring some parameters
@@ -27,6 +52,7 @@ for n = 1:size(ints_table)
                              'dataformat', 'ascii',...
                              'srate', samplingRate);
     elseif get(handles.radioEDF, 'Value')
+        arqedf = ints_table{n, 9};
         EEG = pop_biosig(arqedf, 'rmeventchan', 'off');
     end
 
@@ -49,9 +75,9 @@ for n = 1:size(ints_table)
     EEG = eeg_checkset(EEG);
     if get(handles.radioEDF, 'Value')
         if get(handles.radioEDA, 'Value')
-            EEG = pop_select(EEG, 'time', blockrange, 'channel', 'RGP');
+            EEG = pop_select(EEG, 'time', blockrange, 'channel', {'EMG-RGP'});
         elseif get(handles.radioEMG, 'Value')
-            EEG = pop_select(EEG, 'time', blockrange, 'channel', 'EMG-RGP');
+            EEG = pop_select(EEG, 'time', blockrange, 'channel', {'EMG-RGP'});
         end
     elseif get(handles.radioASCII, 'Value')
         EEG = pop_select(EEG, 'time', blockrange);
@@ -68,15 +94,39 @@ for n = 1:size(ints_table)
     close(h);
 
     h = msgbox('Exporting dataset...');
-    exportASCII = strcat(get(handles.editOutput, 'String'), '\', arqsetname, '.ascii')
+    exportASCII = strcat(get(handles.editOutput, 'String'), '\', arqsetname, '.ascii');
     EEG = pop_export(EEG, exportASCII, 'elec', 'off');
     close(h);
 
-    % TODO Add mathematics
+    % Calculating and saving the parameters
+    h = msgbox('Calculating parameters...');
+    if get(handles.radioEMG, 'Value')
+        emgASCII = load(exportASCII);
+        % Because of the way EEGLab exports the file, one has to ask for the 2nd line
+        emgASCII = emgASCII(2,:);
+        [emgm, emgsd, emgvar, emgrms, emgrmsN, powtotal] = emgfunc(emgASCII);
+        emgm = replace_dot(emgm);
+        emgsd = replace_dot(emgsd);
+        emgvar = replace_dot(emgvar);
+        emgrms = replace_dot(emgrms);
+        emgrmsN = replace_dot(emgrmsN);
+        powtotal = replace_dot(powtotal);
+    elseif get(handles.radioEDA, 'Value')
+        edaASCII = load(exportASCII);
+        edaASCII = edaASCII(2,:);
+        [rgpm, rgpsd, rgpvar, scl, scr] = rgpfunc(edaASCII);
+        rgpm = replace_dot(rgpm);
+        rgpsd = replace_dot(rgpsd);
+        rgpvar = replace_dot(rgpvar);
+        scl = replace_dot(scl);
+        scr = replace_dot(scr);
+    end
+    close(h);
 
     % Adding file to the processed list
     listset{n} = arqset;
     set(handles.listFiles, 'String', listset);
 end
+fclose(fileID);
 
 disp('DEKITA~! o/')
--- P2lib/math/emgfunc.m
+++ P2lib/math/emgfunc.m
@@ -1,8 +1,4 @@
-function [emgm, emgsd, emgvar, emgrms, emgrmsN, powtotal] = emgfunc(emg)
-
-% This step is just necessary because of the way EEGLab exports the ASCII
-% file. To make this function general, upload the next line on the main file
-ampsemg = emg(2,:)
+function [emgm, emgsd, emgvar, emgrms, emgrmsN, powtotal] = emgfunc(ampsemg)
 
 [g,DIM] = size(ampsemg);
 
@@ -10,8 +6,8 @@ ampsemg = emg(2,:)
 emgm = mean(ampsemg);
 emgsd = std(ampsemg);
 
-for j = 1:DIM
-    ampsemgN(j)=(ampsemg(j)-emgm)/emgsd;
+for k = 1:DIM
+    ampsemgN(k)=(ampsemg(k)-emgm)/emgsd;
 end;
     
 % TODO Check why transposing the matrix is necessary
@@ -24,9 +20,9 @@ emgrms = sqrt(sum(ampsemgN.*conj(ampsemgN))/size(ampsemgN,1));
 emgrmsN = norm(ampsemgN)/sqrt(length(ampsemgN));
 
 %pxx=abs(fft(x,n)).^2/n 
-specdata = abs(fft(ampsemgN.*hamming(length(ampsemgN))))./length(ampsemgN);
+specdata = abs(fft(ampsemgN.*hanning(length(ampsemgN))))./length(ampsemgN);
  
 powtotal = 0;
-for i6 = 1:length(specdata)/2
-    powtotal = powtotal + specdata(i6)^2;
+for m = 1:length(specdata)/2
+    powtotal = powtotal + specdata(m)^2;
 end
