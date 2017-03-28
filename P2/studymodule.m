0c6b701d5b6401520dd9e20875552e146e32d5f8
crs94 <rscamila1994@gmail.com>
2017-03-28 14:49:46 -0300

    STUDY generation is working

--- P2/studymodule.m
+++ P2/studymodule.m
@@ -44,6 +44,12 @@ end
 % End initialization code - DO NOT EDIT
 
 
+addpath ([cd '/eegmodule']);
+javaaddpath('edf.jar');
+if ~is_in_javapath('edf.jar')
+      javaaddpath('edf.jar');
+end
+
 % --- Executes just before studymodule is made visible.
 function studymodule_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
@@ -54,8 +60,13 @@ function studymodule_OpeningFcn(hObject, eventdata, handles, varargin)
 
 % Choose default command line output for studymodule
 handles.output = hObject;
+handles.constants = load_constants();
+add_eeglab_path(get(handles.constants, 'EEGLAB_PATH'));
 
 % Update handles structure
+set(handles.editEEGLab, 'String', handles.constants.get('EEGLAB_PATH'));
+set(handles.editOutput, 'String', strcat(pwd, filesep, 'output'));
+set(handles.figure1, 'Name', 'Study Module');
 guidata(hObject, handles);
 
 % UIWAIT makes studymodule wait for user response (see UIRESUME)
@@ -74,50 +85,7 @@ varargout{1} = handles.output;
 
 
 
-function editFile_Callback(hObject, eventdata, handles)
-% hObject    handle to editFile (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of editFile as text
-%        str2double(get(hObject,'String')) returns contents of editFile as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function editFile_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to editFile (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-% --- Executes on button press in buttonSearch.
-function buttonSearch_Callback(hObject, eventdata, handles)
-% hObject    handle to buttonSearch (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --- Executes on button press in buttonStudy.
-function buttonStudy_Callback(hObject, eventdata, handles)
-% hObject    handle to buttonStudy (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --- Executes on button press in buttonMap.
-function buttonMap_Callback(hObject, eventdata, handles)
-% hObject    handle to buttonMap (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-
+%-----------------------------------------------------------------
 function editEEGLab_Callback(hObject, eventdata, handles)
 % hObject    handle to editEEGLab (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -139,6 +107,15 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
+
+% --- Executes on button press in buttonSearchEEGLab.
+function buttonSearchEEGLab_Callback(hObject, eventdata, handles)
+% hObject    handle to buttonSearchEEGLab (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+EEGPath = uigetdir(cd, 'Select the EEGLab folder');
+set(handles.editEEGLab, 'String', EEGPath);
 
 
 function editOutput_Callback(hObject, eventdata, handles)
@@ -163,18 +140,14 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
 end
 
 
-% --- Executes on button press in buttonSearchEEGLab.
-function buttonSearchEEGLab_Callback(hObject, eventdata, handles)
-% hObject    handle to buttonSearchEEGLab (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
 % --- Executes on button press in buttonSearchOut.
 function buttonSearchOut_Callback(hObject, eventdata, handles)
 % hObject    handle to buttonSearchOut (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+EEGPath = uigetdir(cd, 'Select the EEGLab folder');
+set(handles.editEEGLab, 'String', EEGPath);
 
 
 % --- Executes on button press in buttonParameters.
@@ -182,5 +155,112 @@ function buttonParameters_Callback(hObject, eventdata, handles)
 % hObject    handle to buttonParameters (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+switch (get(handles.buttonParameters, 'Value'))
+    case 1
+        set([ handles.editEEGLab handles.editOutput,...
+              handles.buttonSearchEEGLab handles.buttonSearchOut ],...
+            'Enable', 'on');
+        set(handles.buttonParameters, 'String', 'Save Parameters');
+    otherwise
+        set([ handles.editEEGLab handles.editOutput,...
+              handles.buttonSearchEEGLab handles.buttonSearchOut ],...
+            'Enable', 'off');
+        handles.constants.put('EEGLAB_PATH', ...
+                              get(handles.editEEGLab, 'String'));
+        handles.constants.put('OUTPUT_PATH', ...
+                              get(handles.editOutput, 'String'));
+        save_constants(handles.constants);
+        add_eeglab_path(get(handles.constants, 'EEGLAB_PATH'));
+        set(handles.buttonParameters, 'String', 'Edit Parameters');
+end
+guidata(hObject, handles);
+
+
+%-----------------------------------------------------------------
+function editFile_Callback(hObject, eventdata, handles)
+% hObject    handle to editFile (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of editFile as text
+%        str2double(get(hObject,'String')) returns contents of editFile as a double
+
+if isempty(get(handles.editFile, 'String'))
+    set(handles.buttonStudy, 'Enable', 'off');
+else
+    set(handles.buttonStudy, 'Enable', 'on');
+end
+guidata(hObject, handles);
+
+
+% --- Executes during object creation, after setting all properties.
+function editFile_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to editFile (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in buttonSearch.
+function buttonSearch_Callback(hObject, eventdata, handles)
+% hObject    handle to buttonSearch (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+[filename filepath FileInd] = uigetfile('*.xls', 'Select the data file:');
+
+if ~isequal(filename, 0)
+    outlet = strcat(filepath, filename);
+    set(handles.editFile, 'String', outlet);
+    set(handles.buttonStudy, 'Enable', 'on');
+else
+    return;
+    set(handles.buttonStudy, 'Enable', 'off');
+end
+guidata(hObject, handles);
+
+
+% --- Executes on button press in buttonStudy.
+function buttonStudy_Callback(hObject, eventdata, handles)
+% hObject    handle to buttonStudy (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+ints_table = ler_arq_ints(get(handles.editFile, 'String'));
+
+% Open eeglab:
+[ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
+
+studyName = strcat(get(handles.editOutput, 'String'), filesep, 'Study_', date, '.study');
+[STUDY ALLEEG] = pop_study([ ], [ ]);
+
+for n = 1:size(ints_table)
+   % Adding to STUDY
+    setFile = strcat(get(handles.editOutput, 'String'), filesep, ints_table{n, 11});
+    [STUDY ALLEEG] = std_editset(STUDY, ALLEEG, ...
+                                 'name', studyName, ...
+                                 'filename', strcat('/Study_', date), ...
+                                 'filepath', get(handles.editOutput, 'String'), ...
+                                 'commands', ...
+                                 {'index', n, ...
+                                 'load', setFile, ...
+                                 'subject', ints_table{n, 1}, ...
+                                 'session', ints_table{n, 5}});
+end
+
+set(handles.buttonMap, 'Enable', 'on');
+
+% --- Executes on button press in buttonMap.
+function buttonMap_Callback(hObject, eventdata, handles)
+% hObject    handle to buttonMap (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
 
 
