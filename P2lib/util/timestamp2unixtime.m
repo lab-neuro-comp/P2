6fcd6a28d62e1397746153f95e634dd0af74e5a5
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2017-04-05 11:29:10 -0300

    Deleting useless script

--- P2lib/util/timestamp2unixtime.m
+++
@@ -1,5 +0,0 @@
-function [unixtime] = timestamp2unixtime(timestamp)
-% converts a string in the YYYY-MM-DDTHH:MM:SSZ format to a unix time interger.
-
-year = ;
-date = java.util.Date(year, month, day, hours, minutes, seconds);
