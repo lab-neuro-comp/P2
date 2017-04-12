49aeec5b046354df3c0fd6b98b18ae1a451c8469
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2017-04-12 10:49:18 -0300

    Saving signals on their files

---
+++ etc/corrupt/filterMe.m
@@ -0,0 +1,7 @@
+function [outlet] = filterMe(inlet)
+% TODO Make this magic happen
+% IDEA [apps dtls] = wt(inlet, 4, 'fk4')
+% IDEA apps{end} = 0
+% IDEA outlet = iwt(apps, dtls, 'fk4')
+%
+outlet = inlet;
--- etc/corrupt/performDwtAnalysis.m
+++ etc/corrupt/performDwtAnalysis.m
@@ -7,18 +7,17 @@ infoFile = change_extension(inputFile, 'txt');
 edf = br.unb.biologiaanimal.edf.EDF(inputFile);
 labels = edf.getLabels();
 limit = length(labels);
-signals = java.util.HashMap;
+signals = [ ];
+if isequal(char(labels(end)), 'EDF Annotations')
+	limit = limit-1;
+end
 % TODO For each label, filter the correspondent signal
 parfor n = 1:limit
 	label = labels(n);
 	signal = edf.getSignal(label);
-	signal = filterMe(signal);
-	signals.put(label, signal);
+	signal = filterMe(signal)
+	signals(:,n) = signal; % Requires all signals to have the same size
 end
 % TODO Save the signals on a single file
+dlmwrite(outputFile, signals, 'delimiter', '\t');
 % TODO Create information file
-
-function [outlet] = filterMe(inlet)
-% TODO Make this magic happen
-%
-outlet = inlet;
