482031b9cbe107ab15b051bd47fc6cca94c1666a
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2017-06-21 11:06:01 -0300

    Moving edge trigger recognition procedure to its own script

--- etc/squarewave/getPeaks.m
+++ etc/squarewave/getPeaks.m
@@ -1,6 +1,16 @@
 function [peak] = getPeaks(time, inlet)
 % Gets where are the edge triggers in an `inlet signal`.
 %
+bert = -imag(hilbert(inlet));
+[downbert upbert] = moses(bert, 1);
 
-%¨TODO Write this thing
-peak = inlet;
+peak = [ ];
+note = false;
+for it = (upbert-1);
+	if and(~note, it > 0)
+		peak(end+1) = 1;
+	else
+		peak(end+1) = 0;
+	end
+	note = it > 0;
+end
--- etc/squarewave/main.m
+++ etc/squarewave/main.m
@@ -6,27 +6,11 @@ function main()
 % Simulating square wave
 inlet = 0:0.001:10;
 outlet = (squarewave(inlet, 5)+1)/2;
-
-% Plotting square wave
+figure;
 hold on;
-plot(inlet, outlet);
-bert = -imag(hilbert(outlet));
-[downbert upbert] = moses(bert, 1);
-plot(inlet, downbert, 'r');
-plot(inlet, upbert, 'g');
-
-% Discovering peaks
-result = [ ];
-note = false;
-for it = (upbert-1);
-	if and(~note, it > 0)
-		result(end+1) = 1;
-	else
-		result(end+1) = 0;
-	end
-	note = it > 0;
-end
-plot(inlet, result, 'm');
+squarePeaks = getPeaks(inlet, outlet);
+plot(inlet, outlet, 'b');
+plot(inlet, squarePeaks, 'r');
 hold off;
 
-% TODO Get this peak detection peak algorithm in its own script
+% TODO Test procedure with real data
