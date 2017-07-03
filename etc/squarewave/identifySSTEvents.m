6095dc9ba28e2b6c972e15ba53d0f76270a9ee44
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2017-07-03 10:42:25 -0300

    Planning the output of the SST event identification

--- etc/squarewave/identifySSTEvents.m
+++ etc/squarewave/identifySSTEvents.m
@@ -1,7 +1,17 @@
-function identifySSTEvents(edfFile, csvFile)
-% Identify the main events of a SST test execution containing a DC signal for
+function [images sounds] = identifySSTEvents(edfFile, csvFile)
+% Identifies the main events of a SST test execution containing a DC signal for
 % marking events.
 %
+% Two tables are genereted from this function:
+% - The first one is an array of cells containing the moment when an image
+% appeared; a boolean telling if the user answered that moment correctly or
+% not; and how long the user took to answer that trial.
+% - The second one is an array of cells identifying the trials with sound. They
+% list when the sound happenned; the direction of the arrow; if subject got
+% got that trial right; and the answer latency.
+%
+images = [ ];
+sounds = [ ];
 
 % Reading EDF file
 recording = br.unb.biologiaanimal.edf.EDF(edfFile);
