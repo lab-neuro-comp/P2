d30c27575acebce91accff40c00eb17b81ed25d5
crs94 <rscamila1994@gmail.com>
2017-08-14 10:15:57 -0300

    Now it's possible to run P!! regardless of the MATLAB path

--- protolize.m
+++ protolize.m
@@ -1,12 +1,17 @@
 function protolize()
 % TODO Add doc
 
-addpath(strcat(pwd, filesep, 'P2'));
-addpath(strcat(pwd, filesep, 'P2lib'));
-addpath(strcat(pwd, filesep, 'P2lib', filesep, 'biomed'));
-addpath(strcat(pwd, filesep, 'P2lib', filesep, 'functional'));
-addpath(strcat(pwd, filesep, 'P2lib', filesep, 'math'));
-addpath(strcat(pwd, filesep, 'P2lib', filesep, 'util'));
-javaaddpath(strcat(pwd, filesep, 'P2lib', filesep, 'edf.jar'));
-cd P2
+protopath = which('protolize.m');
+protopath = protopath(1:end-length('protolize.m'));
+
+addpath(protopath);
+addpath(strcat(protopath, 'P2'));
+addpath(strcat(protopath, 'P2lib'));
+addpath(strcat(protopath, 'P2lib', filesep, 'biomed'));
+addpath(strcat(protopath, 'P2lib', filesep, 'functional'));
+addpath(strcat(protopath, 'P2lib', filesep, 'math'));
+addpath(strcat(protopath, 'P2lib', filesep, 'util'));
+javaaddpath(strcat(protopath, 'P2lib', filesep, 'edf.jar'));
+
+cd (strcat(protopath, 'P2'));
 protolize2
